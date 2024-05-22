f277c6b02d77fe3d0bb044630731b3e4afa1dc48
thais-roome <tee.szimanski@gmail.com>
2024-05-22 13:07:40 +0100

    Basic styling

---
+++ src/Weather.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            {" "}
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>{" "}
+        </div>
+      </form>
+      <h1>Lisbon</h1>
+      <ul>
+        <li>Wednesday 12:00</li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+            alt="Cloudy"
+          />
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 43%</li>
+            <li>Humidity: 93%</li>
+            <li>Windy: 9mph</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
