6a8fbebd6eae2cd367559d2ef6cb08ccfdf6e916
thais-roome <tee.szimanski@gmail.com>
2024-06-05 17:31:06 +0100

    fixed any broken warnings on the inspect

--- src/App.js
+++ src/App.js
@@ -9,15 +9,27 @@ export default function App() {
         <Weather defaultCity="paris" />
         <footer>
           This project was coded by {""}
-          <a href="https://www.linkedin.com/in/thais-szimanski-82ab97262/">
+          <a
+            href="https://www.linkedin.com/in/thais-szimanski-82ab97262/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Thais Roome
           </a>
           {""} and is {""}
-          <a href="https://github.com/thais-roome/react-weather-app">
+          <a
+            href="https://github.com/thais-roome/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             {""} open-sourced on GitHub,
           </a>{" "}
           hosted on{" "}
-          <a href="https://app.netlify.com/teams/thais-roome/builds/664dc8450ea540e094152000">
+          <a
+            href="https://app.netlify.com/teams/thais-roome/builds/664dc8450ea540e094152000"
+            target="_black"
+            rel="noopener noreferrer"
+          >
             Netlify
           </a>
           👩🏻‍💻
