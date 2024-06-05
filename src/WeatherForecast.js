8391fb4d0ed3446535d412b8b2c5a41f19328c27
thais-roome <tee.szimanski@gmail.com>
2024-06-05 17:21:17 +0100

    added metric unit on the api Url to change temperature to accurate reading

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -28,6 +28,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
@@ -37,10 +39,9 @@ export default function WeatherForecast(props) {
     let apiKey = "c6f8ef4575250284954db9f4dfa7a996";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
-
     return null;
   }
 }
